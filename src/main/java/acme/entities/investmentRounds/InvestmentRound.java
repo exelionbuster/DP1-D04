ce76b22744b01ab43b52ed827991947ee184dbef
adrferfer <3aafernandezf@gmail.com>
2020-08-15 15:51:13 +0200

    Task 005: InvestmentRound created!

---
+++ src/main/java/acme/entities/investmentRounds/InvestmentRound.java
@@ -0,0 +1,51 @@
+
+package acme.entities.investmentRounds;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class InvestmentRound extends DomainEntity {
+
+	//	Serialisation identifier ------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes ---------------------------------------------
+
+	@NotBlank
+	private String				ticker;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				creationDate;
+
+	@NotBlank
+	private String				kind;
+
+	@NotBlank
+	private String				title;
+
+	@NotBlank
+	private String				description;
+
+	private Money				amount;
+
+	@URL
+	private String				link;
+
+}
